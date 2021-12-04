27e475bf62ced6d67505bad14de61f9e0c4cf76c
anton <anton.z@live.com>
2021-12-04 19:42:05 +0300

    Init

---
+++ Makefile
@@ -0,0 +1,32 @@
+.PHONY: help
+help: ## Показать справку
+	@printf "\033[33m%s:\033[0m\n" 'Available commands'
+	@awk 'BEGIN {FS = ":.*?## "} /^[a-zA-Z1-9_-]+:.*?## / {printf "  \033[32m%-16s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST)
+
+.PHONY: start
+start: ## Сборка и запуск докер контейнеров (для dev)
+	docker-compose -f docker-compose.yaml up -d --force-recreate --build
+
+.PHONY: start-prod
+start-prod: ## Сборка и запуск докер контейнеров (для production)
+	docker-compose up -d --force-recreate --build
+
+.PHONY: stop
+stop: ## Остановка докер контейнеров
+	docker-compose down
+
+.PHONY: clean
+clean: ## Удалить образы и директории
+	docker-compose rm --force --stop
+	rm -rf var/cache
+	rm -rf var/log
+	rm -rf var/rabbitmq
+	rm -rf vendor
+
+.PHONY: logs
+logs: ## Показать логи
+	docker-compose logs -f app
+
+.PHONY: sh
+sh: ## Запустить консоль
+	docker-compose exec app sh
---
+++ docker/php/entrypoint.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+#configure php-fpm
+sed -i -e "s/user = .*/user = $USERNAME/" /etc/php-fpm.d/www.conf && \
+sed -i -e "s/group = .*/group = $USERNAME/" /etc/php-fpm.d/www.conf && \
+sed -i -e 's/listen = .*/listen = 9000/' /etc/php-fpm.d/www.conf && \
+sed -i -e 's/listen.allowed_clients = .*/;/' /etc/php-fpm.d/www.conf && \
+sed -i -e 's/php_admin_value\[error_log\] = .*/php_admin_value\[error_log\] = \/dev\/stderr/' /etc/php-fpm.d/www.conf && \
+echo "catch_workers_output = yes" >> /etc/php-fpm.d/www.conf && \
+echo "decorate_workers_output = no" >> /etc/php-fpm.d/www.conf && \
+sed -i -e 's/pid = .*/pid = \/run\/php-fpm.pid/' /etc/php-fpm.conf && \
+sed -i -e 's/error_log = .*/error_log = \/dev\/stderr/' /etc/php-fpm.conf
+groupadd -g $USERID $USERNAME && useradd -u $USERID -g $USERNAME $USERNAME
+chown -R $USERNAME:$USERNAME /var/lib/php
+chown -R $USERNAME:$USERNAME /var/log/php-fpm
+
+chmod +x /mnt/app/bin/console
+
+if [ "$APP_ENV" = "dev" ]
+then
+    echo "START IN DEV MODE"
+    rm /etc/supervisord.d/*.prod.conf
+    composer install --no-ansi --no-interaction
+else
+    echo "START IN PRODUCTION MODE"
+    rm /etc/supervisord.d/*.dev.conf
+fi
+
+console cache:clear
+#wait-for-it.sh --timeout=300 database:3306
+#console doctrine:migration:migrate --no-ansi --no-interaction
+
+supervisord
